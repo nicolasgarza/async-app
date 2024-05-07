f72ab9ff59cbe761e92d7df6b3b257ca8ff2457a
Nicolas Garza <nicolas.garza@gmail.com>
2024-05-07 18:59:24 -0500

    init

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # Base
+    api_v1_prefix: str
+    debug: bool
+    project_name: str
+    version: str
+    description: str
+
+    # Database
+    db_async_connection_str: str
